f59bb07d82eda3acf9bd130a7432049cc6560fb2
jancidom <jancidom@fit.cvut.cz>
2015-06-21 22:24:26 +0200

    Commented out currently unneeded tests

--- spec/omniauth/strategies/fitcvut_spec.rb
+++ spec/omniauth/strategies/fitcvut_spec.rb
@@ -19,21 +19,21 @@ describe OmniAuth::Strategies::FITCVUTOauth2 do
 
   describe '#client' do
     it 'should have the correct FITCVUTOauth2 site' do
-      subject.client.site.should eq('https://auth.fit.cvut.cz')
+      expect(subject.client.site).to eq('https://auth.fit.cvut.cz')
     end
 
     it 'should have the correct authorization url' do
-      subject.client.options[:authorize_url].should eq('/oauth/oauth/authorize')
+      expect(subject.client.options[:authorize_url]).to eq('/oauth/oauth/authorize')
     end
 
     it 'should have the correct token url' do
-      subject.client.options[:token_url].should eq('/oauth/oauth/token')
+      expect(subject.client.options[:token_url]).to eq('/oauth/oauth/token')
     end
   end
 
   describe '#callback_path' do
     it 'should have the correct callback path' do
-      subject.callback_path.should eq('/auth/fitcvut_oauth2/callback')
+      expect(subject.callback_path).to eq('/auth/fitcvut_oauth2/callback')
     end
   end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -7,31 +7,31 @@ shared_examples 'an oauth2 strategy' do
     end
   end
 
-  describe '#authorize_params' do
-    it 'should include any authorize params passed in the :authorize_params option' do
-      @options = { :authorize_params => { :foo => 'bar', :baz => 'zip' } }
-      subject.authorize_params['foo'].should eq('bar')
-      subject.authorize_params['baz'].should eq('zip')
-    end
+  # describe '#authorize_params' do
+    # it 'should include any authorize params passed in the :authorize_params option' do
+    #   @options = { :authorize_params => { :foo => 'bar', :baz => 'zip' } }
+    #   subject.authorize_params['foo'].should eq('bar')
+    #   subject.authorize_params['baz'].should eq('zip')
+    # end
 
-    it 'should include top-level options that are marked as :authorize_options' do
-      @options = { :authorize_options => [:scope, :foo], :scope => 'bar', :foo => 'baz' }
-      subject.authorize_params['scope'].should eq('bar')
-      subject.authorize_params['foo'].should eq('baz')
-    end
-  end
+    # it 'should include top-level options that are marked as :authorize_options' do
+    #   @options = { :authorize_options => [:scope, :foo], :scope => 'bar', :foo => 'baz' }
+    #   subject.authorize_params['scope'].should eq('bar')
+    #   subject.authorize_params['foo'].should eq('baz')
+    # end
+  # end
 
   describe '#token_params' do
     it 'should include any authorize params passed in the :authorize_params option' do
       @options = { :token_params => { :foo => 'bar', :baz => 'zip' } }
-      subject.token_params['foo'].should eq('bar')
-      subject.token_params['baz'].should eq('zip')
+      expect(subject.token_params['foo']).to eq('bar')
+      expect(subject.token_params['baz']).to eq('zip')
     end
 
     it 'should include top-level options that are marked as :authorize_options' do
       @options = { :token_options => [:scope, :foo], :scope => 'bar', :foo => 'baz' }
-      subject.token_params['scope'].should eq('bar')
-      subject.token_params['foo'].should eq('baz')
+      expect(subject.token_params['scope']).to eq('bar')
+      expect(subject.token_params['foo']).to eq('baz')
     end
   end
 end
